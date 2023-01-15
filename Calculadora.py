1ef1eb201685822186ac27e707f12f69f125d608
Israel <israelmartinezreynoso@gmail.com>
2023-01-15 01:10:26 -0600

    Listado

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,17 @@
-import numpy as np
-print("Calculadora, bienvenido")
-print("Escoge una opción")
+import numpy as numpy
+
+while True:
+    print("""
+    Bienvenido, a la calculadora
+    
+    1) Sumar los dos números
+    2) Restar los dos números
+    3) Multiplicar los dos números
+    4) División
+    5) Exponente
+    6) Raiz
+    7) Seno
+    8) Apagar calculadora
+    """)
+    opcion = int(input("Elige una opción: ") )
+   
